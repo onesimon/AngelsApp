6f262014875250a93fbdf393a02120c41e77c422
onesimon <simone.aristotile@eng.it>
2019-06-27 13:44:29 +0200

    First component

--- angels-app/src/app/app-routing.module.ts
+++ angels-app/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponentComponent } from './home-page-component/home-page-component.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'homePage', component: HomePageComponentComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- angels-app/src/app/app.component.ts
+++ angels-app/src/app/app.component.ts
@@ -1,10 +1,21 @@
 import { Component } from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
+
+  constructor(private router: Router,
+              private activatedRoute: ActivatedRoute){
+  }
+
   title = 'angels-app';
+
+  goToHome(){
+    this.router.navigate(['homePage'], {relativeTo: this.activatedRoute});
+  }
 }
